4e0d417eb29ded193a32c0300987f51d23c7dcf8
Ayash Mohammed <2231@tkmce.ac.in>
2023-04-03 14:38:15 +0530

    Update employee3.sql

--- employee3.sql
+++ employee3.sql
@@ -4,6 +4,15 @@ SELECT * FROM employee WHERE salary IN (SELECT MAX(salary) FROM employee);
 -- Display the details of employees whose salary is equal to the minimum salary of organisation.
 SELECT * FROM employee WHERE salary IN (SELECT MIN(salary) FROM employee);
 
+-- Display all the employees whose designation is same as that of 'Arun"
+
+
+-- Display the emp_no and name of employees who earn more than any employee in dept 1
+
+
+-- Display the empno, name, departments that the departments are same in both the emp and dept
+
+
 -- Display the employee details by implementing left inner join
 SELECT * FROM employee e LEFT JOIN department d ON e.dept_no = d.dept_no;
 
